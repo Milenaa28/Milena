7b50817957d517f05e668384c7acdaf7aea63481
Milenaa28 <00001093097620sp@al.educacao.sp.gov.br>
2024-11-22 11:06:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia'
+    'O que é Geografia?',
+    'Geografia é a ciência que estuda sobre o espaço, a natureza e as atividades humanas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o país com mais população?',
+    'O país com maior população é a China, em seguida vem a Ìndia.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos países existem no mundo?',
+    'Atualmente, existem 195 países no mundo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual região do Brasil passa mais dificuldade?',
+    'A região que enfrenta mais dificuldade é o Nordeste.'
+)
